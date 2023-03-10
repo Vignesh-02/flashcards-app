279c453d91ef3c6fb83d2a7bed461cac5260a918
Vignesh-02 <laserlikefocus000@gmail.com>
2023-03-10 05:50:26 +0530

    changed dev urls to prod ones

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,2 +1,3 @@
-const api = import.meta.env.VITE_API_URL
-export const API_URL = api || 'http://localhost:5002'
+const api = import.meta.env.VITE_API_URL 
+// for development use this -> 'http://localhost:5002'
+export const API_URL =  api
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,52 @@
-import express, { Request, Response} from "express"
-import mongoose from 'mongoose'
-import cors from 'cors'
-import { config } from "dotenv"
-config()
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { config } from "dotenv";
+config();
 
-import Deck from "./models/Deck"
-import deckRoutes from './routes/decks'
+import Deck from "./models/Deck";
+import deckRoutes from "./routes/decks";
 
-console.log(Deck)
-const app = express()
-const PORT=5002
+console.log(Deck);
+const app = express();
+const PORT = 5002;
 
-const site_url = process.env.SITE_URL
+const site_url = process.env.SITE_URL;
 
+const whitelist = ["http://localhost:5173/", site_url];
+
+const corsOptions = {
+    origin: function (
+        origin: string | undefined,
+        callback: (arg0: Error | null, arg1: boolean | undefined) => void
+    ) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"), false);
+        }
+    },
+};
+
+// app.use(cors(corsOptions));
+// For development use this, 'http://localhost:5173'
 app.use(cors({
-    origin: site_url || 'http://localhost:5173'
+    origin: site_url
 }))
-app.use(express.json())
 
-app.use("/decks", deckRoutes)
+app.use(express.json());
 
-app.get("/", (req: Request,res: Response) => {
-    res.send('henlo world')
-})
-app.get("/henlo", (req: Request,res: Response) => {
-    res.send('henlo world')
-})
+app.use("/decks", deckRoutes);
 
-mongoose.connect(process.env.ATLAS_URI!)
-.then(() => {
+app.get("/", (req: Request, res: Response) => {
+    res.send("henlo world");
+});
+app.get("/henlo", (req: Request, res: Response) => {
+    res.send("henlo world");
+});
+
+mongoose.connect(process.env.ATLAS_URI!).then(() => {
     app.listen(PORT, () => {
-        console.log(`server is running on port ${PORT}`)
-    })
-}) 
-
- 
+        console.log(`server is running on port ${PORT}`);
+    });
+});
